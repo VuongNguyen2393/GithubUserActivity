fcc930b1071435ae6c86d6a497fbd1d65a3ac5df
vuongnd9 <nguyendinhvuong.act@gmail.com>
2026-03-09 22:07:48 +0700

    setting call API simple

--- Program.cs
+++ Program.cs
@@ -2,8 +2,26 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        System.Console.Write("> ");
+        var input = Console.ReadLine();
+        if (string.IsNullOrEmpty(input))
+        {
+            System.Console.WriteLine("Invalid Username");
+            return;
+        }
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", "CSharpApp");
+        var response = await client.GetAsync($"https://api.github.com/users/{input}/events");
+        if (response.IsSuccessStatusCode)
+        {
+            var data = await response.Content.ReadAsStringAsync();
+            System.Console.WriteLine(data);
+        }
+        else
+        {
+            System.Console.WriteLine("Invalid Request");
+        }
     }
 }
